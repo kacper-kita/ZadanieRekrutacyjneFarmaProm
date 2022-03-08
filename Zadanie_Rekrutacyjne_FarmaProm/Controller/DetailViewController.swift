e884c4b0f5a9586e707b8de6a3c63775fe41f11c
kacper-kita <kkita.contact@gmail.com>
2022-03-08 13:25:08 +0100

    basic stable version of detailView

---
+++ Zadanie_Rekrutacyjne_FarmaProm/Controller/DetailViewController.swift
@@ -0,0 +1,65 @@
+//
+//  DetailViewController.swift
+//  Zadanie_Rekrutacyjne_FarmaProm
+//
+//  Created by Kacper Kita on 08/03/2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    var data: UserViewModel
+    
+    private lazy var detailView: DetailView = {
+        let detailV = DetailView()
+        return detailV
+    }()
+    
+    init(data: UserViewModel) {
+        self.data = data
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        detailView.userName.text = data.first + " " + data.last
+        detailView.userPhone.text = data.phone
+        detailView.userEmail.text = data.email
+        detailView.userStreet.text = data.street
+        detailView.userCity.text = data.city
+        detailView.userState.text = data.state
+        
+        NetworkManager.shared.getImage(urlString: data.picture) { data in
+            
+            guard let image = data else {return}
+            
+            DispatchQueue.main.sync {
+                self.detailView.userImage.image = UIImage(data: image)
+            }
+        }
+        setupView()
+    }
+    
+    private func setupView() {
+        view.addSubview(detailView)
+        view.backgroundColor = .white
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            detailView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            detailView.topAnchor.constraint(equalTo: view.topAnchor),
+            detailView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            detailView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+    }
+}
